0cc744cef3c2e5de8e025fae17bde438a53fb29f
rjmasikome <rjmasikome@gmail.com>
2017-09-15 17:38:47 +0200

    Extend stackdriver

--- src/ServiceLogger.js
+++ src/ServiceLogger.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const bunyan = require("bunyan");
+const googleBunyan = require("@google-cloud/logging-bunyan");
 
 const RawStream = require("./LoggerRawStream.js");
 const Middlewares = require("./ExpressMiddlewares.js");
@@ -9,7 +10,7 @@ const Middlewares = require("./ExpressMiddlewares.js");
 const LOG_LEVELS = ["TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"];
 
 class ServiceLogger {
-    constructor(loggerName, silence, logDir, productionMode, dockerMode, varKey, logFieldOptions, level, serviceName) {
+    constructor(loggerName, silence, logDir, productionMode, dockerMode, varKey, logFieldOptions, level, serviceName, stackdriver) {
 
         if(typeof loggerName === "object" && arguments.length === 1){
             productionMode = loggerName.production || loggerName.productionMode; //support fallback
@@ -20,6 +21,7 @@ class ServiceLogger {
             logFieldOptions = loggerName.logFieldOptions;
             level = loggerName.level || loggerName.logLevel; //support fallback to older key named "logLevel"
             serviceName = loggerName.serviceName;
+            stackdriver = loggerName.stackdriver;
 
             loggerName = loggerName.name; //last
         }
@@ -37,6 +39,7 @@ class ServiceLogger {
         this.logDir = logDir || "logs";
         this.logLevel = level || (productionMode ? "WARN" : "DEBUG"); //level -> logLevel (dockerconfig cannot set camelcase)
         this.serviceName = serviceName || "undefined";
+        this.stackdriver = stackdriver;
 
         this.skipDebug = this.silence || (this.productionMode &&
             !(this.logLevel === "TRACE" || this.logLevel === "DEBUG"));
@@ -48,14 +51,76 @@ class ServiceLogger {
         }
 
         this._streams = null;
+
         this.LOG = this._createLogger();
 
+        let sdInitText = "";
+        if (this.stackdriver) {
+
+          // Not pretty, but the way to catch authentication error is rewriting google bunyan stream lib
+          // Apparently they create a callback from native code which there's no error handling
+          process.on('unhandledRejection', (reason, p) => {
+            console.log('Unhandled Rejection at:', p, 'reason:', reason);
+          });
+
+          switch(this.stackdriver.scope) {
+
+            case "only":
+              // Make the logger streams to only Google Stackdriver
+              this.LOG = this._createGoogleLogger();
+              break;
+
+            case "both":
+              // Make the logger streams to both to default stream and Google Stackdriver
+              this.GLOG = this._createGoogleLogger();
+              break;
+
+            default:
+              // Return default stream when there's no scope is written
+              // this.LOG is still the same
+              this.stackdriver.scope = "default";
+          }
+
+          sdInitText =  ", stackdriver-log=" + Boolean(this.stackdriver) +
+                        ", stream-scope=" + this.stackdriver.scope;
+
+        }
+
         this.LOG.info("[log4bro] Logger is: in-prod=" + this.productionMode +
+            sdInitText +
             ", in-docker:" + this.dockerMode +
             ", level=" + this.logLevel +
             ", skipDebug=" + this.skipDebug);
 
         this.setGlobal();
+
+    }
+
+    _createGoogleLogger() {
+
+      let logger;
+
+      try {
+
+        logger = bunyan.createLogger({
+          "name": this.loggerName,
+          "streams": [
+            googleBunyan(
+              Object.assign({}, this.stackdriver, {logName: this.loggerName})
+            )
+            .stream(this.logLevel)
+          ],
+          "src": false
+        });
+
+      } catch(e) {
+
+        this.LOG.error("Unable to create Stackdriver Stream, switching to default. Reason:", e);
+        logger = this._createLogger();
+      }
+
+
+      return logger;
     }
 
     _createLogger() {
@@ -83,10 +148,11 @@ class ServiceLogger {
         }
 
         return bunyan.createLogger({
-            "name": this.loggerName,
-            "streams": this._streams,
-            "src": false
+          "name": this.loggerName,
+          "streams": this._streams,
+          "src": false
         });
+
     }
 
     createChild(defaultAdditionalFields) {
@@ -209,8 +275,15 @@ class ServiceLogger {
             };
 
             Object.assign(message, additionalFields);
+
+            if (this.GLOG)
+              this.GLOG[level](message);
+
             return this.LOG[level](message);
         }
+
+        if (this.GLOG)
+          this.GLOG[level](additionalFields, message);
 
         return this.LOG[level](additionalFields, message);
     }
